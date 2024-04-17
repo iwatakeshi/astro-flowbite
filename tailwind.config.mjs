91a1e1d85dfc9dd825620a02bdcb0be987ecadee
Takeshi <tak@prismify.co>
2024-04-16 21:36:57 -0600

    Update tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#17202A",
+        secondary: "#ecc94b",
+        transparent: "transparent",
+        current: "currentColor",
+        slate: colors.slate,
+        stone: colors.stone,
+        blue: colors.blue,
+        green: colors.green,
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
